205412da9f656f527bf7fe63d40004160e7dd74a
Mamour <coolertall@gmail.com>
2016-06-13 17:43:19 +0000

    Autre methode pour preparer

--- src/com/pizzeria/Pizza.java
+++ src/com/pizzeria/Pizza.java
@@ -14,6 +14,14 @@ public abstract class Pizza {
         System.out.print("\nConstructeur de : ");
     }
 
+
+    /**
+     * autre methode pour implementer la methode preparer
+     * public void preparer(string type){
+     * pate = faabriqueIngrdient.creerPate(type);
+     * sauce = fabriqueIngredient.creerSauce(type);
+     * }
+     */
     public abstract void preparer();
 
     public abstract void emballer();
